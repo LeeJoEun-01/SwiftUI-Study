3c8090f586cfdb256e92bec097074c3fd1e48423
LeeJoEun-01 <joeun1005@naver.com>
2023-03-29 00:35:19 +0900

    Ch02. Building Lists and Navigation
    https://velog.io/@joni_dev/SwiftUI-Chapter-1-Building-Lists-and-Navigation

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
-            .resizable()
-            .frame(width: 200, height: 200)
-            //.scaledToFit()
+        image
             .clipShape(Circle())
             .overlay{
                 Circle().stroke(.white, lineWidth: 4)
@@ -23,6 +22,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
---
+++ Landmarks/Landmarks/Views/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 이조은 on 2023/03/16.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .ignoresSafeArea(edges: .all)
+            .onAppear{
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -166.166_868))
+    }
+}
