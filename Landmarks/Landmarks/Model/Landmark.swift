edf6bf3bbc6c30e61b40a2bb913e2f26299319c0
LeeJoEun-01 <joeun1005@naver.com>
2023-05-11 15:26:31 +0900

    Chapter4. Framework Integration
    4-1. Interfacing with UIKit

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -30,11 +30,15 @@ struct Landmark: Hashable, Codable, Identifiable {
         Image(imageName)
     }
     
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+    
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D (
             latitude: coordinates.latitude, longitude: coordinates.longitude
-            )
+        )
     }
     
     struct Coordinates: Hashable,Codable {
