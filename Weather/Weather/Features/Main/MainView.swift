25cbd6d7f122f57d0e8b3215ebded87760cf05f9
LeeJoEun-01 <joeun1005@naver.com>
2024-09-21 21:47:17 +0900

    [feat] 날씨 정보 UI 추가

--- Weather/Weather/Features/Main/MainView.swift
+++ Weather/Weather/Features/Main/MainView.swift
@@ -15,14 +15,44 @@ enum Tab {
 
 struct MainView: View {
     @State var selectedTab: Tab = .main
+
+    @State var location: String = "Seoul"
+    @State var temparature: Int = 21
+    @State var weather: String = "Partly Cloudy"
+    @State var high: Int = 23
+    @State var low: Int = 17
+
     var body: some View {
         ZStack{
-            ScrollView{
-
-            }
             Image("MainBG")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
+            ScrollView{
+                Text(location)
+                    .font(.system(size: 37))
+                    .foregroundStyle(.white)
+                    .padding(.top, 34)
+
+                Text("\(temparature)°")
+                    .font(.system(size: 102))
+                    .fontWeight(.thin)
+                    .foregroundStyle(.white)
+
+                Text(weather)
+                    .font(.system(size: 24))
+                    .foregroundStyle(.white)
+
+                HStack(spacing: 12, content: {
+                    Text("H:\(high)°")
+                        .font(.system(size: 21))
+                        .fontWeight(.medium)
+                        .foregroundStyle(.white)
+                    Text("L:\(low)°")
+                        .font(.system(size: 21))
+                        .fontWeight(.medium)
+                        .foregroundStyle(.white)
+                })
+            }
         }
     }
 }
