2f49860a22bdb64942e65f823cb41b05fc2278c5
LeeJoEun-01 <joeun1005@naver.com>
2024-09-27 21:36:43 +0900

    [feat] offset, position, padding 공부

--- Weather/Weather/StudyView.swift
+++ Weather/Weather/StudyView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct StudyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        ZStack {
+//            Image(systemName: "heart")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .foregroundStyle(.red)
+//                .frame(width: 50, height: 50)
+//                .padding(.top, 50)
+//                .offset(x:20, y: 30)
+//
+//            Image(systemName: "heart")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 50, height: 50)
+//                .padding(.top, 50)
+//                .position(CGPoint(x: 100.0, y: 100.0))
+//        }
+
+        VStack {
+            // 1.
+            Text("ABCDEFG")
+                .padding(.bottom, 15)
+            // 2.
+            Text("ABCDEFG")
+                .padding([.leading, .bottom], 15)
+            // 3.
+            Text("ABCDEFG")
+                .padding(EdgeInsets(top: 0, leading: 15, bottom: 15, trailing: 0))
+            // 4
+            Text("ABCDEFG")
+        }
+
     }
 }
 
